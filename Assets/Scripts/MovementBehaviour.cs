cd9dc6654ebabffb7d0014250ecbc0694add0d0f
victorVerlaenen <vverlaenen@gmail.com>
2022-10-04 17:41:50 +0200

    Fixed the temp jump program
    made the jump proigram work by adding a jump method in the movement behaviour

--- Assets/Scripts/MovementBehaviour.cs
+++ Assets/Scripts/MovementBehaviour.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class MovementBehaviour : MonoBehaviour
 {
     [SerializeField] private float _movementSpeed = 1.0f;
-
     private Rigidbody2D _rigidbody;
+    private static readonly Vector2 _jumpVelocity = new Vector2(0, 10);
 
     private Vector2 _desiredMovementDirection = Vector2.zero;
     public Vector2 DesiredMovementDirection
@@ -30,5 +30,10 @@ public class MovementBehaviour : MonoBehaviour
 
         var initialVelocity = _rigidbody.velocity;
         _rigidbody.velocity = new Vector2(movement.x, initialVelocity.y);
+    }
+
+    public void Jump()
+    {
+        _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _jumpVelocity.y);
     }
 }
--- Assets/Scripts/PushingProgram.cs
+++ Assets/Scripts/PushingProgram.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class PushingProgram : Program
 {
-    private static readonly Vector2 _jumpVelocity = new Vector2(0, 1000);
 
     public override void HandleAbility()
     {
-        // Doesn't work
-        _player.Movement.DesiredMovementDirection = new Vector2(_player.Movement.DesiredMovementDirection.x, _jumpVelocity.y);
+        _player.Movement.Jump();
     }
 }
